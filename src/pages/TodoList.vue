db3d4fd192e126c2da3b3825c5fd0d798069e69f
RahafAl0 <ra.al9760@gmail.com>
2024-04-07 17:33:09 +0300

    edit add tasks and delete tasks

--- src/pages/TodoList.vue
+++ src/pages/TodoList.vue
@@ -24,27 +24,22 @@
       class="bg-white"
       separator
       bordered>
-
-
       <q-item
         v-for="(task, index) in tasks"
         :key="task.title"
-        @click="task.done = !task.done"
+        @click="toggleTask(index)"
         clickable
         :class="{ 'done bg-blue-1' : task.done }"
         v-ripple>
-
         <q-item-section avatar>
           <q-checkbox
             v-model="task.done"
             class="no-pointer-events"
-            color= "accent" />
+            color="accent" />
         </q-item-section>
-
         <q-item-section>
           <q-item-label>{{task.title}}</q-item-label>
         </q-item-section>
-
         <q-item-section
           v-if="task.done"
           side>
@@ -56,9 +51,7 @@
             icon="delete" />
         </q-item-section>
       </q-item>
-
     </q-list>
-
     <div
       v-if="!tasks.length"
       class="no-tasks absolute-center">
@@ -73,56 +66,47 @@
   </q-page>
 </template>
 
-
 <script setup lang="ts">
-import { defineProps } from 'vue';
-// import { ref } from 'vue';
+import { ref } from 'vue';
 
-const props = defineProps<{
-  tasks: Array<{
-    title: string;
-    done: boolean;
-  }>;
-}>();
-
-const { tasks = props.tasks || [
+const tasks = ref([
   { title: 'Learn Vue 3', done: false },
   { title: 'Learn Quasar', done: false },
   { title: 'Build an app', done: false },
   { title: 'Deploy it', done: false },
   { title: 'Share it', done: false },
-] } = props;
+]);
 
-let newTask = '';
-
-
-
+const newTask = ref('');
 
 const addTask = () => {
-  tasks.push({
-    title: newTask,
-    done: false
-  });
-  newTask = '';
+  if (newTask.value.trim() !== '') {
+    tasks.value.push({
+      title: newTask.value,
+      done: false
+    });
+    newTask.value = '';
+  }
 };
 
 const deleteTask = (index: number) => {
-  const deletedTask = tasks[index].title;
-  tasks.splice(index, 1);
+  const deletedTask = tasks.value[index].title;
+  tasks.value.splice(index, 1);
   console.log('Task deleted:', deletedTask);
 };
 
+const toggleTask = (index: number) => {
+  tasks.value[index].done = !tasks.value[index].done;
+};
 </script>
 
 <style scoped>
-.done {
-  .q-item__label {
-    text-decoration: line-through;
-    color: #bbb;
-  }
+.done .q-item__label {
+  text-decoration: line-through;
+  color: #bbb;
 }
+
 .no-tasks {
   opacity: 0.5;
 }
-
 </style>
